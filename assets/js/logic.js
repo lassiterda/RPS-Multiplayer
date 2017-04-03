dd664733f5bef64acfa04d6a2636022827bebd46
David L <lassiterda@gmail.com>
2017-04-03 13:46:22 -0400

    changes to class/id naming conventions for standardiation in HTML and CSS, rewrote player selection Modal logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,37 +1,36 @@
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDop8OcfQH0pLIwu8Zh9gtADKO1ULcrWTk",
-    authDomain: "rps-multiplayer-dec0e.firebaseapp.com",
-    databaseURL: "https://rps-multiplayer-dec0e.firebaseio.com",
-    projectId: "rps-multiplayer-dec0e",
-    storageBucket: "rps-multiplayer-dec0e.appspot.com",
-    messagingSenderId: "488083470223"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyDop8OcfQH0pLIwu8Zh9gtADKO1ULcrWTk",
+  authDomain: "rps-multiplayer-dec0e.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-dec0e.firebaseio.com",
+  projectId: "rps-multiplayer-dec0e",
+  storageBucket: "rps-multiplayer-dec0e.appspot.com",
+  messagingSenderId: "488083470223"
+};
+firebase.initializeApp(config);
 
-  //Firebase variables
-  var database = firebase.database();
-  var userListRef = database.ref("USERS_ONLINE");
-  var gameStateRef = database.ref("GAME_STATE");
-  var userRef = userListRef.push({player: "unknown"});
+//Firebase variables
+var database = firebase.database();
+var userListRef = database.ref("USERS_ONLINE");
+var gameStateRef = database.ref("GAME_STATE");
+var userRef = userListRef.push({player: "unknown"});
 
-  //Game State Variables
-  var thisUser = userRef.key;
-
-  //DOM variables
-  var btnShoot = $("#button-shoot");
+//Game State Variables
+var thisUser = userRef.key;
+var one;
+var two;
+//DOM variables
+var btnShoot = $("#button-shoot");
 
   // sync the game State with firebase
   gameStateRef.on("value", function(snap) {
-    $("#player1-panel h3").text(snap.val().one.name)
-    $("#player1-wins").text(snap.val().one.wins);
-    $("#player1-losses").text(snap.val().one.losses);
+    $("#one-panel h3").text(snap.val().one.name)
+    $("#one-wins").text(snap.val().one.wins);
+    $("#one-losses").text(snap.val().one.losses);
 
-    $("#player2-panel h3").text(snap.val().two.name)
-    $("#player2-wins").text(snap.val().two.wins);
-    $("#player2-losses").text(snap.val().two.losses);
-
-
+    $("#two-panel h3").text(snap.val().two.name)
+    $("#two-wins").text(snap.val().two.wins);
+    $("#two-losses").text(snap.val().two.losses);
   });
 
   //creates/disconnects users when they load/close the page
@@ -40,30 +39,36 @@
       userRef.onDisconnect().remove()
     }
     userRef.set({present: true})
+
   });
 
 //Controls the Player selection Modal, displaying different options depending on the number of users/active players
-userListRef.once("value", function(snap) {
-  //loop through the active users to see how many 'players' there are
-  var otherPlayers = Object.keys(snap.val()).filter(function(ele){
-    return snap.val()[ele].player != "unknown"
-  });
-  //If no other player has made a selection...
-  if (otherPlayers.length === 0) {
-    $("#modal").modal('show');
-    $("#selection-area").removeClass("hidden");
-  }
-  //If there's already another player in the game...
-  else if (otherPlayers.length === 1){
-      otherPlayers = Object.keys(snap.val()).filter(function(ele){
-        return snap.val()[ele].player != "unknown"
-      })[0];
-    $(".modal-body div").removeClass("btn-group");
-      $(".player-btn[data-player='" + snap.val()[otherPlayers].player + "']").addClass("hidden")
-    $("#modal").modal('show');
-    $("#selection-area").removeClass("hidden");
-  }
+database.ref().once("value", function(snap) {
+  var gameState = snap.val().GAME_STATE;
+  var activeUsers = Object.keys(snap.val().USERS_ONLINE)
 
+  //If no other player has made a selection... (checks to see if an Online user is listed as Player one or two)
+  if (activeUsers.indexOf(gameState.one.userKey) === -1 &&
+      activeUsers.indexOf(gameState.two.userKey) === -1) {
+       $("#modal").modal('show');
+       $("#selection-area").removeClass("hidden");
+       console.log("a")
+  }
+  //If player one hasn't beed picked
+  else if (activeUsers.indexOf(gameState.one.userKey) != -1) {
+
+       $(".modal-body div").removeClass("btn-group");
+       $(".player-btn[data-player='one']").addClass("hidden");
+       $("#modal").modal('show');
+       console.log("b")
+  }
+  //If player two  hasn't beed picked
+  else if (activeUsers.indexOf(gameState.two.userKey) != -1) {
+       $(".modal-body div").removeClass("btn-group");
+       $(".player-btn[data-player='two']").addClass("hidden");
+       $("#modal").modal('show');
+       console.log("c")
+  }
 });
 
   //Click handler for the player selection buttons
@@ -119,10 +124,12 @@ userListRef.once("value", function(snap) {
 
     //grab user selection string (Rock, Paper, or Scissors)
     var selection = btnSelected.attr("alt");
-
+    var player = $(this).attr("data-player")
     //Update the User's selection in fb
-    gameStateRef.child(thisUser).child(selection).update({selection: selection})
-    //
+    gameStateRef.child(player).update({selection: selection})
+
+    $("#one-content").html("<p>Ready!</p>")
+
     btnSelected.attr("data-selected", "false");
     btnSelected.removeClass("selected");
     btnSelected.css("filter", "invert(87%)")
